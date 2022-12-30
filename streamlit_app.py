131c4fe3e8721766b066c961938570e4ac9ca1c7
Miguel <miguelantonio.otoya@unmsm.edu.pe>
2022-12-30 14:04:55 -0500

    subiendo alguno de los modelos

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import home,EDA,transformacion,knn,svc  # import your app modules here
+
+
+app = MultiApp()
+
+st.title("""  #Inteligencia de negocios - Equipo A """)
+
+# Add all your application here
+
+
+app.add_app('Home',home.app)
+app.add_app('Análisis Exploratorio de Datos',EDA.app)
+app.add_app('Transformación de datos',transformacion.app)
+app.add_app('SVC',svc.app)
+app.add_app('KNN',knn.app)
+
+
+
+# The main app
+app.run()
